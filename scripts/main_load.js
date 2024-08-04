8ad74d8f1fe56f71cf0c0f6f4be588e37c8157a3
Thunderbell <smurforino444@gmail.com>
2024-08-04 03:01:09 +0200

    styles

--- scripts/main_load.js
+++ scripts/main_load.js
@@ -22,9 +22,13 @@ function loadItems() {
                     var lvlContent = segments[2] ? segments[2].trim() : '';
 
                     // Create a text element with the modified text
+                    var lvl = document.createElement('p');
                     var text = document.createElement('p');
+                    
+                    lvl.textContent = lvlContent;
+                    lvl.classList.add("item_lvl")
                     text.textContent = textContent;
-                    div.appendChild(text);
+                    text.classList.add("item_text")
 
                     // Create an image element
                     var img = document.createElement('img');
@@ -41,7 +45,11 @@ function loadItems() {
                     } 
             
                     img.classList.add('item-image');
+                    
+                    // put all together
                     div.appendChild(img);
+                    div.appendChild(lvl);
+                    div.appendChild(text);
 
                     // Add the div to the item list
                     itemList.appendChild(div);
