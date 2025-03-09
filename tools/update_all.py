1a678612a410b72b4d4664cd044d35470ddb8b5c
Thunderbell <smurforino444@gmail.com>
2025-03-09 02:32:10 +0100

    .

--- tools/update_all.py
+++ tools/update_all.py
@@ -2,13 +2,13 @@ from update_affiliates import affiliates_update
 # from update_header_footer import
 from update_news import news_update
 
-print(f"Update Affiliates {'-'*20}")
+print(f"Update Affiliates {'-'*50}")
 
 affiliates_update()
 
-print(f"Update Header & Footer {'-'*15}")
+print(f"Update Header & Footer {'-'*45}")
 
 
-print(f"Update News {'-'*26}")
+print(f"Update News {'-'*56}")
 
 news_update()
